85e10c1842da462839ce60494a1f78b7474dd768
bong <iostalkboy@gamil.com>
2017-07-30 12:02:30 +0800

    add objc-runtime

---
+++ objc4-680-runtime/debug-objc/main.m
@@ -0,0 +1,19 @@
+
+//  main.m
+//  debug-objc
+//
+//  Created by closure on 2/24/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        id objec = [[NSObject alloc] init];
+        [objec class];
+    }
+    return 0;
+}
